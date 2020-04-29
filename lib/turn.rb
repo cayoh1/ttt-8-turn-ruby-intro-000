432d4b21560baefda283e046e6a0ae6a13283562
Christine Ayoh <christineayoh9@gmail.com>
2020-04-29 05:22:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,7 @@ def turn(board)
     while valid_move?(board,index) == false
       puts "Please enter 1-9:"
       input = gets.strip()
+      index = input_to_index(input)
       if valid_move?(board,index)
         break
     end
